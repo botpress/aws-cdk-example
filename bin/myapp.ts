4d172db639ebbf718379e3ff90360877d0965a6a
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2021-01-13 15:04:07 -0500

    vpc stack

--- bin/myapp.ts
+++ bin/myapp.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { MyappStack } from '../lib/myapp-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { VpcStack } from "../lib/vpc-stack";
 
 const app = new cdk.App();
-new MyappStack(app, 'MyappStack');
+new VpcStack(app, "VpcStack");
---
+++ lib/vpc-stack.ts
@@ -0,0 +1,10 @@
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
+
+export class VpcStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "VPC", { natGateways: 1 });
+  }
+}
